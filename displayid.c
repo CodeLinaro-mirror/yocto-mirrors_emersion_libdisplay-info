CodeLinaro-mirror/yocto-mirrors_emersion_libdisplay-info

commit e4e0986580e62875bc63cdb4b1cff428e710e2ef

displayid: Add support for type IV timings
We currently have no support for HDMI VIC codes but CTA VIC and DMT
should get printed correctly.

Signed-off-by: Sebastian Wick <sebastian.wick@redhat.com>

displayid.c
@@ -347,6 +347,85 @@ parse_type_ii_timing_block(struct di_displayid *displayid,
 }
 
 static bool
+parse_type_iv_timing_block(struct di_displayid *displayid,
+			   struct di_displayid_type_iv_timing *type_iv_timing,
+			   const uint8_t *data, size_t size)
+{
+	uint8_t flags;
+	uint8_t type;
+	size_t i;
+	uint8_t code;
+	struct di_dmt_code *dmts = NULL;
+	struct di_cta_vic *cta_vics = NULL;
+	struct di_hdmi_vic *hdmi_vics = NULL;
+
+	check_data_block_revision(displayid, data,
+				  "Video Timing Modes Type 4 - Short Timings Data Block",
+				  1);
+
+	flags = get_bit_range(data[0x01], 5, 3);
+	if (flags != 0) {
+		add_failure(displayid,
+			    "Video Timing Modes Type 4 - Short Timings Data Block: Unexpected flags (0x%02x).",
+			    flags);
+	}
+
+	type = get_bit_range(data[0x01], 7, 6);
+	switch (type) {
+	case DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_DMT:
+	case DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_VIC_CTA:
+	case DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_VIC_HDMI:
+		type_iv_timing->type = type;
+		break;
+	default:
+		add_failure(displayid,
+			    "Video Timing Modes Type 4 - Short Timings Data Block: Reserved Timing Code Type 0x%02x.",
+			    type);
+		break;
+	}
+
+	type_iv_timing->codes_len = size - DISPLAYID_DATA_BLOCK_HEADER_SIZE;
+	if (type_iv_timing->codes_len == 0) {
+		add_failure(displayid,
+			    "Video Timing Modes Type 4 - Short Timings Data Block: No Timing Code present.");
+		return true;
+	}
+
+	switch (type) {
+	case DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_DMT:
+		dmts = calloc(type_iv_timing->codes_len, sizeof(*dmts));
+
+		for (i = 0; i < type_iv_timing->codes_len; i++) {
+			code = data[DISPLAYID_DATA_BLOCK_HEADER_SIZE + i];
+			dmts[i] = (struct di_dmt_code) { .code = code };
+		}
+		break;
+	case DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_VIC_CTA:
+		cta_vics = calloc(type_iv_timing->codes_len, sizeof(*cta_vics));
+
+		for (i = 0; i < type_iv_timing->codes_len; i++) {
+			code = data[DISPLAYID_DATA_BLOCK_HEADER_SIZE + i];
+			cta_vics[i] = (struct di_cta_vic) { .code = code };
+		}
+		break;
+	case DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_VIC_HDMI:
+		hdmi_vics = calloc(type_iv_timing->codes_len, sizeof(*hdmi_vics));
+
+		for (i = 0; i < type_iv_timing->codes_len; i++) {
+			code = data[DISPLAYID_DATA_BLOCK_HEADER_SIZE + i];
+			hdmi_vics[i] = (struct di_hdmi_vic) { .code = code };
+		}
+		break;
+	}
+
+	type_iv_timing->dmts = dmts;
+	type_iv_timing->cta_vics = cta_vics;
+	type_iv_timing->hdmi_vics = hdmi_vics;
+
+	return true;
+}
+
+static bool
 parse_tiled_topo_block(struct di_displayid *displayid,
 		       struct di_displayid_tiled_topo_priv *priv,
 		       const uint8_t *data, size_t size)
@@ -565,9 +644,14 @@ parse_data_block(struct di_displayid *displayid, const uint8_t *data,
 		if (!parse_type_iii_timing_block(displayid, data_block, data, data_block_size))
 			goto error;
 		break;
+	case DI_DISPLAYID_DATA_BLOCK_TYPE_IV_TIMING:
+		if (!parse_type_iv_timing_block(displayid,
+						&data_block->type_iv_timing,
+						data, data_block_size))
+			goto error;
+		break;
 	case DI_DISPLAYID_DATA_BLOCK_PRODUCT_ID:
 	case DI_DISPLAYID_DATA_BLOCK_COLOR_CHARACT:
-	case DI_DISPLAYID_DATA_BLOCK_TYPE_IV_TIMING:
 	case DI_DISPLAYID_DATA_BLOCK_VESA_TIMING:
 	case DI_DISPLAYID_DATA_BLOCK_CEA_TIMING:
 	case DI_DISPLAYID_DATA_BLOCK_TIMING_RANGE_LIMITS:
@@ -727,6 +811,11 @@ destroy_data_block(struct di_displayid_data_block *data_block)
 		for (i = 0; i < data_block->type_iii_timings_len; i++)
 			free(data_block->type_iii_timings[i]);
 		break;
+	case DI_DISPLAYID_DATA_BLOCK_TYPE_IV_TIMING:
+		free((struct di_dmt_code *) data_block->type_iv_timing.dmts);
+		free((struct di_cta_vic *) data_block->type_iv_timing.cta_vics);
+		free((struct di_hdmi_vic *) data_block->type_iv_timing.cta_vics);
+		break;
 	default:
 		break; /* Nothing to do */
 	}
@@ -810,6 +899,15 @@ di_displayid_data_block_get_type_iii_timings(const struct di_displayid_data_bloc
 		return NULL;
 	}
 	return (const struct di_displayid_type_iii_timing *const *) data_block->type_iii_timings;
+}
+
+const struct di_displayid_type_iv_timing *
+di_displayid_data_block_get_type_iv_timing(const struct di_displayid_data_block *data_block)
+{
+	if (data_block->tag != DI_DISPLAYID_DATA_BLOCK_TYPE_IV_TIMING) {
+		return NULL;
+	}
+	return &data_block->type_iv_timing;
 }
 
 const struct di_displayid_data_block *const *

di-edid-decode/displayid.c
@@ -307,6 +307,29 @@ print_displayid_type_iii_timing_block(const struct di_displayid_data_block *data
 		print_displayid_type_iii_timing(timings[i]);
 }
 
+static void
+print_displayid_type_iv_timing_block(const struct di_displayid_data_block *data_block)
+{
+	size_t i;
+	const struct di_displayid_type_iv_timing *timing;
+
+	timing = di_displayid_data_block_get_type_iv_timing(data_block);
+	switch (timing->type) {
+	case DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_DMT:
+		for (i = 0; i < timing->codes_len; i++)
+			print_dmt_timing_code(timing->dmts[i]);
+		break;
+	case DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_VIC_CTA:
+		for (i = 0; i < timing->codes_len; i++)
+			print_cta_vic_timing(timing->cta_vics[i]);
+		break;
+	case DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_VIC_HDMI:
+		for (i = 0; i < timing->codes_len; i++)
+			print_hdmi_vic_timing(timing->hdmi_vics[i]);
+		break;
+	}
+}
+
 static const char *
 displayid_product_type_name(enum di_displayid_product_type type)
 {
@@ -417,6 +440,9 @@ print_displayid(const struct di_displayid *displayid)
 			break;
 		case DI_DISPLAYID_DATA_BLOCK_TYPE_III_TIMING:
 			print_displayid_type_iii_timing_block(data_block);
+			break;
+		case DI_DISPLAYID_DATA_BLOCK_TYPE_IV_TIMING:
+			print_displayid_type_iv_timing_block(data_block);
 			break;
 		default:
 			break; /* Ignore */

include/displayid.h
@@ -78,6 +78,9 @@ struct di_displayid_data_block {
 	struct di_displayid_type_iii_timing *type_iii_timings[DISPLAYID_MAX_TYPE_III_TIMINGS + 1];
 	size_t type_iii_timings_len;
 
+	/* Used for TYPE_IV_TIMING */
+	struct di_displayid_type_iv_timing type_iv_timing;
+
 	/* Used for DISPLAY_PARAMS */
 	struct di_displayid_display_params_priv display_params;
 

include/libdisplay-info/displayid.h
@@ -12,6 +12,10 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include <libdisplay-info/dmt.h>
+#include <libdisplay-info/hdmi-vic.h>
+#include <libdisplay-info/cta-vic.h>
+
 /**
  * DisplayID data structure.
  */
@@ -344,6 +348,45 @@ struct di_displayid_type_iii_timing {
  */
 const struct di_displayid_type_iii_timing *const *
 di_displayid_data_block_get_type_iii_timings(const struct di_displayid_data_block *data_block);
+
+/**
+ * Type IV timing, Timing Code Type
+ */
+enum di_displayid_type_iv_timing_code_type {
+	/* DMT Timing Code */
+	DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_DMT = 0,
+	/* CTA/CEA VIC Timing Code */
+	DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_VIC_CTA = 1,
+	/* HDMI VIC Timing Code */
+	DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_VIC_HDMI = 2,
+};
+
+/**
+ * Type IV timing, defined in section 4.4.4.
+ */
+struct di_displayid_type_iv_timing {
+	/* Type of codes */
+	enum di_displayid_type_iv_timing_code_type type;
+	/* Number of codes in one of dmts, cta_vics or hdmi_vics, depending on the value of type*/
+	size_t codes_len;
+	/* DMT Timing Codes, if type is DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_DMT, NULL otherwise */
+	const struct di_dmt_code *dmts;
+	/* CTA VICs, if type is DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_VIC_CTA, NULL otherwise */
+	const struct di_cta_vic *cta_vics;
+	/* HDMI VICs, if type is DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_VIC_HDMI, NULL otherwise */
+	const struct di_hdmi_vic *hdmi_vics;
+};
+
+/**
+ * Get type IV timing from a DisplayID data block.
+ *
+ * The returned array is NULL-terminated.
+ *
+ * Returns NULL if the data block tag isn't
+ * DI_DISPLAYID_DATA_BLOCK_TYPE_IV_TIMING.
+ */
+const struct di_displayid_type_iv_timing *
+di_displayid_data_block_get_type_iv_timing(const struct di_displayid_data_block *data_block);
 
 /**
  * Get DisplayID data blocks.

commit 53b10095cf1793f73a73a99f30710eee3684594b

displayid: Handle failed allocations
Forgot that we try to handle allocation failure. Fix it up.

Fixes: e4e0986 ("displayid: Add support for type IV timings")
Signed-off-by: Sebastian Wick <sebastian.wick@redhat.com>

displayid.c
@@ -394,6 +394,8 @@ parse_type_iv_timing_block(struct di_displayid *displayid,
 	switch (type) {
 	case DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_DMT:
 		dmts = calloc(type_iv_timing->codes_len, sizeof(*dmts));
+		if (!dmts)
+			return false;
 
 		for (i = 0; i < type_iv_timing->codes_len; i++) {
 			code = data[DISPLAYID_DATA_BLOCK_HEADER_SIZE + i];
@@ -402,6 +404,8 @@ parse_type_iv_timing_block(struct di_displayid *displayid,
 		break;
 	case DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_VIC_CTA:
 		cta_vics = calloc(type_iv_timing->codes_len, sizeof(*cta_vics));
+		if (!cta_vics)
+			return false;
 
 		for (i = 0; i < type_iv_timing->codes_len; i++) {
 			code = data[DISPLAYID_DATA_BLOCK_HEADER_SIZE + i];
@@ -410,6 +414,8 @@ parse_type_iv_timing_block(struct di_displayid *displayid,
 		break;
 	case DI_DISPLAYID_TYPE_IV_TIMING_CODE_TYPE_VIC_HDMI:
 		hdmi_vics = calloc(type_iv_timing->codes_len, sizeof(*hdmi_vics));
+		if (!hdmi_vics)
+			return false;
 
 		for (i = 0; i < type_iv_timing->codes_len; i++) {
 			code = data[DISPLAYID_DATA_BLOCK_HEADER_SIZE + i];